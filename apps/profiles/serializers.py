093ba759d502493e159898662bd041e1e1927436
ivangol739 <ivangol739@gmail.com>
2025-03-12 14:56:58 +0300

    добавлены эндпоинты профиля

--- apps/profiles/serializers.py
+++ apps/profiles/serializers.py
@@ -7,3 +7,14 @@ class ProfileSerializer(serializers.Serializer):
     email = serializers.EmailField(read_only=True)
     avatar = serializers.ImageField(required=False)
     account_type = serializers.CharField(read_only=True)
+
+
+class ShippingAddressSerializer(serializers.Serializer):
+    id = serializers.UUIDField(read_only=True)
+    full_name = serializers.CharField(max_length=500)
+    email = serializers.EmailField()
+    phone = serializers.CharField(max_length=12)
+    address = serializers.CharField(max_length=1000)
+    city = serializers.CharField(max_length=100)
+    country = serializers.CharField(max_length=100)
+    zipcode = serializers.IntegerField()
--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.common.utils import set_dict_attr
-from apps.profiles.serializers import ProfileSerializer
+from apps.profiles.models import ShippingAddress
+from apps.profiles.serializers import ProfileSerializer, ShippingAddressSerializer
 
 
 class ProfileView(APIView):
@@ -78,3 +79,99 @@ class ProfileView(APIView):
         user.is_active = False
         user.save()
         return Response(data={"message": "User Account Deactivated"})
+
+
+class ShippingAddressesView(APIView):
+    serializer_class = ShippingAddressSerializer
+
+    @extend_schema(
+        summary="Shipping Addresses Fetch",
+        description="""
+        This endpoint returns all shipping addresses associated with a user.
+    """,
+        tags=tags,
+    )
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        shipping_addresses = ShippingAddress.objects.filter(user=user)
+        serializer = self.serializer_class(shipping_addresses, many=True)
+        return Response(data=serializer.data)
+
+    @extend_schema(
+        summary="Create Shipping Address",
+        description="""
+            This endpoint allows a user to create a shipping address.
+        """,
+        tags=tags,
+    )
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        shipping_address, _ = ShippingAddress.objects.get_or_create(user=user, **data)
+        serializer = self.serializer_class(shipping_address)
+        return Response(data=serializer.data, status=201)
+
+
+class ShippingAddressViewID(APIView):
+    serializer_class = ShippingAddressSerializer
+
+    def get_object(self, user, shipping_id):
+        shipping_address = ShippingAddress.objects.get_or_none(user=user, id=shipping_id)
+        return shipping_address
+
+    @extend_schema(
+        summary="Shipping Address Fetch ID",
+        description="""
+        This endpoint returns a single shipping address associated with a user.
+    """,
+        tags=tags,
+    )
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        shipping_address = self.get_object(user, kwargs["id"])
+        if not shipping_address:
+            return Response(data={"message": "Shipping Address does not exist!"}, status=404)
+        serializer = self.serializer_class(shipping_address)
+        return Response(data=serializer.data)
+
+    @extend_schema(
+        summary="Update Shipping Address ID",
+        description="""
+        This endpoint allows a user to update his/her shipping address.
+    """,
+        tags=tags,
+    )
+
+    def put(self, request, *args, **kwargs):
+        user = request.user
+        shipping_address = self.get_object(user, kwargs["id"])
+        if not shipping_address:
+            return Response(data={"message": "Shipping Address does not exist!"}, status=404)
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        shipping_address = set_dict_attr(shipping_address, data)
+        shipping_address.save()
+        serializer = self.serializer_class(shipping_address)
+        return Response(data=serializer.data, status=200)
+
+    @extend_schema(
+        summary="Delete Shipping Address ID",
+        description="""
+            This endpoint allows a user to delete his/her shipping address.
+        """,
+        tags=tags,
+    )
+    def delete(self, request, *args, **kwargs):
+        user = request.user
+        shipping_address = self.get_object(user, kwargs["id"])
+        if not shipping_address:
+            return Response(data={"message": "Shipping Address does not exist!"}, status=404)
+        shipping_address.delete()
+        return Response(data={"message": "Shipping address deleted successfully"}, status=200)
