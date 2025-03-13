08f005767bd45c4eb53260b8d8433709192b038b
ivangol739 <ivangol739@gmail.com>
2025-03-14 02:33:51 +0300

    эндпоинты по продуктам

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from apps.shop.views import CategoriesView
+from apps.shop.views import CategoriesView, ProductView, ProductsView, ProductsByCategoryView, ProductsBySellerView
 
 urlpatterns = [
     path("categories/", CategoriesView.as_view()),
+    path("categories/<slug:slug>/", ProductsByCategoryView.as_view()),
+    path("sellers/<slug:slug>/", ProductsBySellerView.as_view()),
+    path("products/", ProductsView.as_view()),
+    path("products/<slug:slug>/", ProductView.as_view()),
 ]
