86b59434dd95fc6e6f7f4bf48eaec7f2589e7d72
ivangol739 <ivangol739@gmail.com>
2025-03-14 01:51:55 +0300

    Перевод БД на postgres, доавлены энжпоинты для изменения, удаления продуктов

--- apps/sellers/urls.py
+++ apps/sellers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.sellers.views import SellersView, ProductsBySellerView
+from apps.sellers.views import SellersView, ProductsBySellerView,SellerProductView
 
 urlpatterns = [
     path("", SellersView.as_view()),
     path("products/", ProductsBySellerView.as_view()),
+    path("products/<slug:slug>/", SellerProductView.as_view()),
 ]
--- apps/shop/serializers.py
+++ apps/shop/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework import serializers
 
-
+#сериализатор для категорий
 class CategorySerializer(serializers.Serializer):
     name = serializers.CharField()
     slug = serializers.SlugField(read_only=True)
     image = serializers.ImageField()
 
-
+#сериализатор используется для сериализации данных о продавце (магазине)
 class SellerShopSerializer(serializers.Serializer):
     name = serializers.CharField(source="business_name")
     slug = serializers.CharField()
     avatar = serializers.CharField(source="user.avatar")
 
 
+#сериализатор предназначен для сериализации данных о продукте.
 class ProductSerializer(serializers.Serializer):
     seller = SellerShopSerializer()
     name = serializers.CharField()
@@ -26,7 +27,7 @@ class ProductSerializer(serializers.Serializer):
     image2 = serializers.ImageField(required=False)
     image3 = serializers.ImageField(required=False)
 
-
+# сериализатор, похожий на ProductSerializer, но предназначен для создания продукта
 class CreateProductSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     desc = serializers.CharField()
