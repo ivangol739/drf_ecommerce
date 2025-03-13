92a7d3556c0f8a4ccaa77775db52ee1ba811e7a5
ivangol739 <ivangol739@gmail.com>
2025-03-13 23:49:03 +0300

    эндпоинты продавца и магазина

---
+++ apps/shop/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.Serializer):
+    name = serializers.CharField()
+    slug = serializers.SlugField(read_only=True)
+    image = serializers.ImageField()
+
+
+class SellerShopSerializer(serializers.Serializer):
+    name = serializers.CharField(source="business_name")
+    slug = serializers.CharField()
+    avatar = serializers.CharField(source="user.avatar")
+
+
+class ProductSerializer(serializers.Serializer):
+    seller = SellerShopSerializer()
+    name = serializers.CharField()
+    slug = serializers.SlugField()
+    desc = serializers.CharField()
+    price_old = serializers.DecimalField(max_digits=10, decimal_places=2)
+    price_current = serializers.DecimalField(max_digits=10, decimal_places=2)
+    category = CategorySerializer()
+    in_stock = serializers.IntegerField()
+    image1 = serializers.ImageField()
+    image2 = serializers.ImageField(required=False)
+    image3 = serializers.ImageField(required=False)
+
+
+class CreateProductSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    desc = serializers.CharField()
+    price_current = serializers.DecimalField(max_digits=10, decimal_places=2)
+    category_slug = serializers.CharField()
+    in_stock = serializers.IntegerField()
+    image1 = serializers.ImageField()
+    image2 = serializers.ImageField(required=False)
+    image3 = serializers.ImageField(required=False)
